f61b75130b459ea8ca0b79ee665f7a17826e0581
rajdeep-dabral <mail.rajdeepdabral@gmail.com>
2021-03-11 11:43:34 +0530

    Completed

--- restFullWebServicePart2/src/main/java/dabral/rajdeep/restFullWebServicePart2/RestFullWebServicePart2Application.java
+++ restFullWebServicePart2/src/main/java/dabral/rajdeep/restFullWebServicePart2/RestFullWebServicePart2Application.java
@@ -8,6 +8,9 @@ import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.Locale;
@@ -16,6 +19,9 @@ import java.util.Locale;
 //@Configuration
 @EnableSwagger2
 public class RestFullWebServicePart2Application {
+	public static final ApiInfo API_INFO =  new ApiInfo(
+			"User Management" , "Perform CURD Operations related to User" , "1.0",
+			"urn:tos" , "Rajdeep Dabral","Apache" , "www.apache.org");
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestFullWebServicePart2Application.class, args);
@@ -26,5 +32,10 @@ public class RestFullWebServicePart2Application {
 		AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
 		localeResolver.setDefaultLocale(Locale.US);
 		return localeResolver;
+	}
+
+	@Bean
+	public Docket api(){
+		return new Docket(DocumentationType.SWAGGER_2).apiInfo(API_INFO );
 	}
 }
